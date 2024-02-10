f51e7e8e2f5e1a5f0d373ce7c89e10012d882454
dmegaritis <dmgrei3@gmail.com>
2024-02-10 11:16:57 +0000

    init file

---
+++ ML/__init__.py
@@ -0,0 +1,6 @@
+from ML.NN_Classifier import NN_Classifier
+from ML.LinearRegression import Linear_Regression
+from ML.LogisticRegression import Logistic_Regression
+from ML.NN_Regression import NN_Regression
+
+__all__ = ['NN_Classifier', 'Linear_Regression', 'Logistic_Regression', 'NN_Regression']
