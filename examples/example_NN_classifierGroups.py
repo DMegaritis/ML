9ef5355e563b8dabcde7609d50be5981368db622
dmegaritis <dmgrei3@gmail.com>
2024-06-18 10:27:20 +0100

    Example added for NN_ClassifierGroups

---
+++ examples/example_NN_classifierGroups.py
@@ -0,0 +1,38 @@
+from ML.NN_ClassifierGroups import NN_ClassifierGroups
+
+'''
+This is an example on how to use the NN_Classifier class to train a Multi-layer Perceptron (MLP) Regressor model using
+example data. This version predicts class values (categorical variables).
+'''
+
+# Specifying parameters
+file_paths = ['/Users/dimitrismegaritis/PycharmProjects/MLTable/examples/example_data_classification_groups.csv']
+table_path = '/Users/dimitrismegaritis/PycharmProjects/MLTable/examples/results_NN_ClassifierGroups.csv'
+target_variables = ['target']
+input_feature_columns_continuous = ['a', 'b', 'c']
+first_heading = 'clinical_variable'
+second_heading = 'disease_status'
+levels = 3
+neurons = 500
+splits = 5
+scaler = 'yes'
+early_stopping = False
+group = ['ID']
+
+# Creating an instance of NN_Classifier
+nn_classifier = NN_ClassifierGroups(
+    file_paths=file_paths,
+    table_path=table_path,
+    target_variables=target_variables,
+    input_feature_columns_continuous=input_feature_columns_continuous,
+    first_heading=first_heading,
+    second_heading=second_heading,
+    levels=levels,
+    neurons=neurons,
+    splits=splits,
+    scaler=scaler,
+    early_stopping=early_stopping,
+    group=group
+)
+
+nn_classifier.train()
