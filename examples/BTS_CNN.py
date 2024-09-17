217c26a66d6f0315086828c31e4c976b65658a0d
dmegaritis <dmgrei3@gmail.com>
2024-09-17 09:05:57 +0100

    Example on how to run the 1D Convolutional neural network with local paths

---
+++ examples/BTS_CNN.py
@@ -0,0 +1,14 @@
+from ML.CNN import CNN_Classifier
+import numpy as np
+
+# Specifying parameters
+file_path = '/Users/dimitrismegaritis/Documents/BTS 2024/aggregated/cleaned_aggregated_BTS24.npy'
+features = np.load(file_path)
+file_path = '/Users/dimitrismegaritis/Documents/BTS 2024/aggregated/target_BTS24.npy'
+target = np.load(file_path)
+groups = np.arange(features.shape[0])
+
+# Creating an instance of CNN_Classifier
+cnn_classifier = CNN_Classifier(features=features, target=target, groups=groups, n_splits=5, epochs=20, batch_size=32)
+cnn_classifier.train()
+
