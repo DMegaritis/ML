1b423668bcdaf0f438e6eb944f8471438cde9c12
DMegaritis <dmgrei3@gmail.com>
2024-09-22 18:53:27 +0100

    Locan WN paths

--- examples/BTS_CNN.py
+++ examples/BTS_CNN.py
@@ -2,9 +2,9 @@ from ML.CNN import CNN_Classifier
 import numpy as np
 
 # Specifying parameters
-file_path = '/Users/dimitrismegaritis/Documents/BTS 2024/aggregated/cleaned_aggregated_BTS24.npy'
+file_path = r'C:\Users\klch3\PycharmProjects\MLTable\data/cleaned_aggregated_BTS24.npy'
 features = np.load(file_path)
-file_path = '/Users/dimitrismegaritis/Documents/BTS 2024/aggregated/target_BTS24.npy'
+file_path = r'C:\Users\klch3\PycharmProjects\MLTable\data/target_BTS24.npy'
 target = np.load(file_path)
 groups = np.arange(features.shape[0])
 
