3860ad199f39453914bb6a06f412ced633ad70a4
DMegaritis <dmgrei3@gmail.com>
2024-09-22 19:24:55 +0100

    Local paths Wn

--- examples/BTS_CNN.py
+++ examples/BTS_CNN.py
@@ -2,13 +2,13 @@ from ML.CNN import CNN_Classifier
 import numpy as np
 
 # Specifying parameters
-file_path = r'C:\Users\klch3\PycharmProjects\MLTable\data/cleaned_aggregated_BTS24.npy'
+file_path = r'C:\Users\klch3\PycharmProjects\MLTable\data/cleaned_aggregated_BTS24_first_60_all_vars.npy'
 features = np.load(file_path)
-file_path = r'C:\Users\klch3\PycharmProjects\MLTable\data/target_BTS24.npy'
+file_path = r'C:\Users\klch3\PycharmProjects\MLTable\data/target_BTS24_first_60_all_vars.npy'
 target = np.load(file_path)
 groups = np.arange(features.shape[0])
 
 # Creating an instance of CNN_Classifier
-cnn_classifier = CNN_Classifier(features=features, target=target, groups=groups, n_splits=5, epochs=20, batch_size=32)
+cnn_classifier = CNN_Classifier(features=features, target=target, groups=groups, n_splits=5, epochs=50, batch_size=32)
 cnn_classifier.train()
 
